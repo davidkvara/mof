403ef05472068b36f1f56185fefeae323b4ffe67
davidkvara <davekvaratskhelia@gmail.com>
2018-09-03 19:07:26 +0400

    sw: cache only woff2 fonts

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: "dist/",
-  globPatterns: ["**/*.{woff2,woff,ttf,png,html,svg,js,css}"],
+  globPatterns: ["**/*.{woff2,png,html,svg,js,css}"],
   swDest: "./dist/sw.js",
   clientsClaim: true,
   skipWaiting: true
